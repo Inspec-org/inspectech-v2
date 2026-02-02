a683631ec1ef83325a60fe5fba1d2632a0c4c860
mlbench-lhr <mlbenchpvtltd@gmail.com>
2026-02-02 11:34:53 +0500

    empty commit

--- src/app/(full-width-pages)/(auth)/forget-password/page.tsx
+++ src/app/(full-width-pages)/(auth)/forget-password/page.tsx
@@ -1,7 +1,6 @@
 import ForgetForm from "@/components/auth/ForgetForm";
 import { Metadata } from "next";
 
-
 // export const metadata: Metadata = {
 //   title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
 //   description: "This is Next.js Signin Page TailAdmin Dashboard Template",
